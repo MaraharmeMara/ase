92fedb08409ddca0ee58c8a5d058b7b4fdb358a0
Ivan Cheremisin <fess932@gmail.com>
2023-08-04 12:37:07 +0300

    fix

--- scripts/qrcodegen.js
+++ scripts/qrcodegen.js
@@ -1,11 +1,12 @@
 
-var QRCode = require('qrcode');
-var fs = require('fs');
-var path = require('path');
+const QRCode = require('qrcode');
+const fs = require('fs');
+const path = require('path');
 
 // Source and destination folder paths
 const sourceFolderPath = './models';
 const destinationFolderPath = './qrcodes';
+const prefix = "https://stctuotanto.fi/360vr/ase/?qr="
 
 // Read the files from the source folder
 fs.readdir(sourceFolderPath, function (err, files) {
@@ -16,18 +17,19 @@ fs.readdir(sourceFolderPath, function (err, files) {
 
   // Loop through the files
   files.forEach(function (file) {
+    const filename = path.parse(file).name
     // Create the full file paths
-    var sourceFilePath = path.join(sourceFolderPath, file);
-    var destinationFilePath = path.join(destinationFolderPath, `${file}.png`);
+    const qrcode = `${prefix}${filename}`;
+    const destinationFilePath = path.join(destinationFolderPath, `${file}.png`);
 
     // Generate QR code from the 3D model filename
-    QRCode.toFile(destinationFilePath, file, function (err) {
+    QRCode.toFile(destinationFilePath, qrcode, function (err) {
       if (err) {
         console.error('Error generating QR code:', err);
         return;
       }
 
-      console.log(`QR code generated for ${file}`);
+      console.log(`QR code generated for ${qrcode}`);
     });
   });
 });
