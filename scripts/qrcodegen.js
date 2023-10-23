1b7d77d5778b083008b9fc3a29cac69d9eea58e9
Ivan Cheremisin <fess932@gmail.com>
2023-10-23 11:30:40 +0300

    fix

--- scripts/qrcodegen.js
+++ scripts/qrcodegen.js
@@ -1,23 +1,22 @@
-
-const QRCode = require('qrcode');
-const fs = require('fs');
-const path = require('path');
+const QRCode = require("qrcode");
+const fs = require("fs");
+const path = require("path");
 
 // Source and destination folder paths
-const sourceFolderPath = './models';
-const destinationFolderPath = './qrcodes';
-const prefix = "https://stctuotanto.fi/360vr/AR-web/?qr="
+const sourceFolderPath = "./models";
+const destinationFolderPath = "./qrcodes";
+const prefix = "https://stctuotanto.fi/360vr/AR-web/?qr=";
 
 // Read the files from the source folder
 fs.readdir(sourceFolderPath, function (err, files) {
   if (err) {
-    console.error('Error reading files from the source folder:', err);
+    console.error("Error reading files from the source folder:", err);
     return;
   }
 
   // Loop through the files
   files.forEach(function (file) {
-    const filename = path.parse(file).name
+    const filename = path.parse(file).name;
     // Create the full file paths
     const qrcode = `${prefix}${filename}`;
     const destinationFilePath = path.join(destinationFolderPath, `${file}.png`);
@@ -25,7 +24,7 @@ fs.readdir(sourceFolderPath, function (err, files) {
     // Generate QR code from the 3D model filename
     QRCode.toFile(destinationFilePath, qrcode, { scale: 25 }, function (err) {
       if (err) {
-        console.error('Error generating QR code:', err);
+        console.error("Error generating QR code:", err);
         return;
       }
 
